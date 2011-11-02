e2fc47004601b52a2b3f775e5e2083b0b1e86e03
Alexandre Gravier <al.gravier@gmail.com>
2011-11-02 17:42:00 +0800

    Add pynn_utils and tests for abstracted common manipulations.

--- src/attention_net/scheduling/nettraining.py
+++ src/attention_net/scheduling/nettraining.py
@@ -5,3 +5,13 @@ network.
 
 from SimPy.Simulation import Process
 
+from common.pynn_utils import get_weights, set_weights
+from common.utils import log_tick, LOGGER
+
+
+
+def self_org_learn(population, 
+                   samples, 
+                   presentation_cycles,
+                   stop_condition):
+    pass
--- src/attention_net/scheduling/pynn_scheduling.py
+++ src/attention_net/scheduling/pynn_scheduling.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python2
 """ Utilities and algorithms to integrate PyNN and SimPy
 """
+
+import SimPy.Simulation
+
+SIMPY_END_T = 0
+
+def configure_scheduling():
+    SimPy.Simulation.initialize()
+
+def run_simulation():
+    SimPy.Simulation.simulate(until=SIMPY_END_T)
+
